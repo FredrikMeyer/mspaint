824e6456755a089014c0f13b7f1ca3416ca9f67b
Fredrik Meyer <hrmeyer@gmail.com>
2022-05-01 13:34:01 +0200

    Fix correct colors

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -5,37 +5,43 @@ export default function ColorPicker({
 }: {
   setCurrentColor: (c: string) => void;
 }) {
-  const colors = [
-    "white",
-    "grey",
-    "red",
-    "yellow",
-    "cyan",
-    "blue",
-    "magenta",
-    "lightyellow",
-    "lightgreen",
-    "lightskyblue",
-    "orchid",
+  const row1 = [
+    "rgb(255, 255, 255)",
+    "rgb(191, 191, 191)",
+    "rgb(233, 50, 35)", // light red
+    "rgb(255, 253, 84)", // bright yellow
+    "rgb(117, 249, 76)", // neon green
+    "rgb(115, 251, 253)", // light blue
+    "rgb(0, 30, 245)", // blue
+    "rgb(234, 60, 247)", // magenta
+    "rgb(255, 255, 146)", // yellow-ish
+    "rgb(117, 250, 142)", // yellow-green
+    "rgb(160, 252, 253)", // sky-blue
+    "rgb(127, 131, 247)", // purple
+    "rgb(234, 53, 127)", // pink
+    "rgb(239, 134, 80)", // orange
   ];
 
   const row2 = [
-    "black",
-    "darkgrey",
-    "darkred",
-    "goldenrod",
-    "seagreen",
-    "cornflowerblue",
-    "darkblue",
-    "mediumpurple",
-    "wheat",
-    "darkolivegreen",
-    "dodgerblue",
+    "rgb(0, 0, 0)", // black
+    "rgb(128, 128, 128)", // dark grey
+    "rgb(117, 20, 12)", // dark red
+    "rgb(128, 127, 38)", // dark yellow
+    "rgb(55, 125, 34)", // dark neon gren
+    "rgb(54, 126, 127)", // dark teal
+    "rgb(0, 10, 123)", // dark blue
+    "rgb(117, 25, 124)", // dark magenta
+    "rgb(128, 127, 73)", // wheat
+    "rgb(24, 63, 63)", // dark green
+    "rgb(51, 129, 247)", // blue sky dark
+    "rgb(22, 64, 124)", // dark dark blue sky
+    "rgb(57, 15, 123)", // dark purple
+    "rgb(120, 66, 21)", // brown
   ];
   return (
     <div className="main-cc-color-picker">
       <div className="main-cc-color-picker-row">
-        {colors.map((c, i) => (
+        {row1.map((c, i) => (
           <div
             key={i}
             style={{ backgroundColor: c }}
